47cd3080715ec60ee02405e7ce44dc6105a829d1
Carolina Amorim <carolinaoa1@al.insper.edu.com>
2026-03-24 16:36:20 -0300

    Initial commit

---
+++ src/main/java/br/insper/app_corrida/AppCorridaApplication.java
@@ -0,0 +1,13 @@
+package br.insper.app_corrida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCorridaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCorridaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/insper/app_corrida/AppCorridaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.insper.app_corrida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCorridaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
